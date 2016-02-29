c6f593feb10e528b076df174f304f2ba99d5e7db
MikhailTodes <mikhailtodes2016@u.northwestern.edu>
2016-02-28 20:26:21 -0600

    Added a canny edge filtered version of video as well as video

---
+++ canny_edge.cpp
@@ -0,0 +1,34 @@
+//compile code -> g++ canny_edge.cpp -o canny_edge.e `pkg-config --cflags --libs opencv`
+
+#include <iostream>
+#include <fstream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp> 
+#include "opencv2/opencv.hpp"
+#include <time.h>
+
+using namespace std;
+using namespace cv;
+
+
+int main(int, char**)
+{
+    VideoCapture cap("sherman.mp4"); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
+
+    Mat edges;
+    namedWindow("edges",1);
+    for(;;)
+    {
+        Mat frame;
+        cap >> frame; // get a new frame from camera
+        cvtColor(frame, edges, CV_BGR2GRAY);
+        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        Canny(edges, edges, 0, 30, 3);
+        imshow("edges", edges);
+        if(waitKey(30) >= 0) break;
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+}
