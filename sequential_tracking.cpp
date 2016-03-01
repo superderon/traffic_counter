b2cbc5878bc40d70c7ae4b157b609a71aa8f9f33
MikhailTodes <mikhailtodes2016@u.northwestern.edu>
2016-03-01 12:28:52 -0600

    Added a dilation and tracking all contours

--- sequential_tracking.cpp
+++ sequential_tracking.cpp
@@ -11,63 +11,81 @@ using namespace std;
 using namespace cv;
 
 string parseInt(int number){//convert ints to string for printing
-	//this function has a number input and string output
-	std::stringstream ss;
-	ss << number;
-	return ss.str();
+  //this function has a number input and string output
+  std::stringstream ss;
+  ss << number;
+  return ss.str();
 }
 
 void locateCoordsOfDetectedObject(Mat thresholdImage, Mat &cameraFeed){//locate the coordinates of the detected objects
-	bool detectedObj = false;//boolean to keep track of detected objects
-	Mat temp;
-        Rect rectToBindObject = Rect(0, 0, 0, 0);//Create a rectangle to bind the object
-	thresholdImage.copyTo(temp);
-	vector< vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-	findContours(temp, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);	
-	if (contours.size()>0)detectedObj = true;//If the contours are not empty then an object has been detected
-	else detectedObj = false;
+  bool detectedObj = false;//boolean to keep track of detected objects
+  Mat temp;
+  Rect rectToBindObject = Rect(0, 0, 0, 0);//Create a rectangle to bind the object
+  thresholdImage.copyTo(temp);
+  vector< vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+  findContours(temp, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);	
+  if (contours.size()>0)detectedObj = true;//If the contours are not empty then an object has been detected
+  else detectedObj = false;
 
-	if (detectedObj){//when an object is detected		
-		vector< vector<Point> > largestContourVec;
-		largestContourVec.push_back(contours.at(contours.size() - 1));//Find largest contour at end of the contour vector		
-		rectToBindObject = boundingRect(largestContourVec.at(0));//Put binding rect around the object
-		int xpos = rectToBindObject.x + rectToBindObject.width / 2;//find position x and y of the centre of the object
-		int ypos = rectToBindObject.y + rectToBindObject.height / 2;
-		//draw circle around object and write coords to screen
-		circle(cameraFeed, Point(xpos, ypos), 20, Scalar(0, 255, 0), 2);
-		putText(cameraFeed, "(" + parseInt(xpos) + "," + parseInt(ypos) + ")", Point(xpos, ypos), 1, 1, Scalar(255, 0, 0), 2);
-	}
+  if (detectedObj){//when an object is detected		
+    for (int i = contours.size()-1; i>1; i--)
+      {
+        vector< vector<Point> > largestContourVec;
+	largestContourVec.push_back(contours.at(i));//Find all contours		
+	rectToBindObject = boundingRect(largestContourVec.at(0));//Put binding rect around the object
+	
+	int pt1x = rectToBindObject.x;
+	int pt1y = rectToBindObject.y;
+	int pt2x = rectToBindObject.x + rectToBindObject.width;
+	int pt2y = rectToBindObject.y + rectToBindObject.height;
+
+	int xpos = rectToBindObject.x + rectToBindObject.width / 2;//find position x and y of the centre of the object
+	int ypos = rectToBindObject.y + rectToBindObject.height / 2;
+
+	rectangle(cameraFeed, Point(pt1x,pt1y), Point(pt2x,pt2y), Scalar(0, 255, 0), 1);
+	putText(cameraFeed, "(" + parseInt(xpos) + "," + parseInt(ypos) + ")", Point(xpos, ypos), 1, 1, Scalar(255, 0, 0), 2);
+      }
+  }
 }
 
 int main(int, char**)
 {
-    VideoCapture cap("sherman.mp4");
-    if(!cap.isOpened()) 
-        return -1;
+  VideoCapture cap("sherman.mp4");
+  if(!cap.isOpened()) 
+    return -1;
 
-    Mat edges;
-    namedWindow("Tracking",1);
-    namedWindow("Threshold",2);
+  Mat edges;
+  namedWindow("Tracking",1);
+  namedWindow("Threshold",2);
 
-    for(;;)
+  for(;;)
     {
       Mat frame, frame2, diff, grey1, grey2, thresh;
-        cap >> frame;
-	cap >> frame2;
-	cv::cvtColor(frame, grey1, COLOR_BGR2GRAY);//convert to greyscale
-	cv::cvtColor(frame2, grey2, COLOR_BGR2GRAY);//convert to greyscale
-        //GaussianBlur(frame, frame, Size(15,15), 3,3);
-	//GaussianBlur(frame2, frame2, Size(15,15), 3, 3);
-	absdiff(grey1, grey2, diff);
-	threshold(diff, thresh, 130, 255, THRESH_BINARY);//(Mat grayScaleImage, Mat outputSave, sensitvityValue, maxVal, typeOutput)
+      cap >> frame;
+      cap >> frame2;
+      cv::cvtColor(frame, grey1, COLOR_BGR2GRAY);//convert to greyscale
+      cv::cvtColor(frame2, grey2, COLOR_BGR2GRAY);//convert to greyscale
+      GaussianBlur(grey1, grey1, Size(15,15),3,3);
+      GaussianBlur(grey2, grey2, Size(15,15), 3,3);
+      absdiff(grey1, grey2, diff);
+      threshold(diff, thresh, 70, 255, THRESH_BINARY);//(Mat grayScaleImage, Mat outputSave, sensitvityValue, maxVal, typeOutput)
 
-	locateCoordsOfDetectedObject(thresh, frame2);
+      // Create a structuring element
+       int erosion_size = 4;  
+       Mat element = getStructuringElement(cv::MORPH_CROSS,
+              cv::Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+              cv::Point(erosion_size, erosion_size) );
 
-        imshow("Tracking", frame2);
-	        imshow("Threshold", thresh);
+       //dilate(thresh, thresh, element, Point(1,1), 5);//input, output, kernal, anchor, iterations
 
-        if(waitKey(30) >= 0) break;
+      locateCoordsOfDetectedObject(thresh, frame2);
+
+      imshow("Tracking", frame2);
+      // imshow("Difference", diff);
+      imshow("Threshold", thresh);
+
+      if(waitKey(30) >= 0) break;
     }
-    return 0;
+  return 0;
 }
